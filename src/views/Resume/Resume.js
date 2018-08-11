b82710a718f74fabe467fe7cb2582bada73987ec
Marcelo <marcelogvallejos@gmail.com>
2018-08-11 14:44:18 -0300

    Adding submodule to src

--- src/views/Resume/Resume.js
+++ src/views/Resume/Resume.js
@@ -4,6 +4,8 @@ import TripList from '../../components/TripList/TripList';
 import TripBox from '../../components/TripBox/TripBox';
 import TripCard from '../../components/TripCard/TripCard';
 
+const store =  require("../../TripPlanningCore/src");
+
 class Resume extends Component{
     constructor(props){
         super(props);
@@ -23,54 +25,6 @@ class Resume extends Component{
 
         />];
         let newTrips = [<TripCard 
-            img = './DSCN3207.JPG'
-            name = "Código rutero"
-            description = "Viaje a lo largo de la arentina codeando y conociendo"
-            cities = {["Sierra de la ventana", "Las grutas"]}
-            places = {["Cerro Bahía Blanca", "Piedras Rojas"]}
-
-        />,
-        <TripCard 
-            img = './DSCN3207.JPG'
-            name = "Código rutero"
-            description = "Viaje a lo largo de la arentina codeando y conociendo"
-            cities = {["Sierra de la ventana", "Las grutas"]}
-            places = {["Cerro Bahía Blanca", "Piedras Rojas"]}
-
-        />,
-        <TripCard 
-            img = './DSCN3207.JPG'
-            name = "Código rutero"
-            description = "Viaje a lo largo de la arentina codeando y conociendo"
-            cities = {["Sierra de la ventana", "Las grutas"]}
-            places = {["Cerro Bahía Blanca", "Piedras Rojas"]}
-
-        />,
-        <TripCard 
-            img = './DSCN3207.JPG'
-            name = "Código rutero"
-            description = "Viaje a lo largo de la arentina codeando y conociendo"
-            cities = {["Sierra de la ventana", "Las grutas"]}
-            places = {["Cerro Bahía Blanca", "Piedras Rojas"]}
-
-        />,
-        <TripCard 
-            img = './DSCN3207.JPG'
-            name = "Código rutero"
-            description = "Viaje a lo largo de la arentina codeando y conociendo"
-            cities = {["Sierra de la ventana", "Las grutas"]}
-            places = {["Cerro Bahía Blanca", "Piedras Rojas"]}
-
-        />,
-        <TripCard 
-            img = './DSCN3207.JPG'
-            name = "Código rutero"
-            description = "Viaje a lo largo de la arentina codeando y conociendo"
-            cities = {["Sierra de la ventana", "Las grutas"]}
-            places = {["Cerro Bahía Blanca", "Piedras Rojas"]}
-
-        />,
-        <TripCard 
             img = './DSCN3207.JPG'
             name = "Código rutero"
             description = "Viaje a lo largo de la arentina codeando y conociendo"
